3ca78c3d664f392fd94c10a54eb3ce7b69779a42
Shubham Singh <shubhammahar1306@gmail.com>
2024-02-15 07:55:38 +0000

    add

--- frontend/src/components/IdealWeightForm/IdealWeightForm.jsx
+++ frontend/src/components/IdealWeightForm/IdealWeightForm.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-
 const IdealWeightForm = () => {
   const [gender, setGender] = useState("");
   const [height, setHeight] = useState("");
